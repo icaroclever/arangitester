06e406e99781b00224874adfbbc634fdf4a1d60f
goncalvesl@gmail.com <goncalvesl@gmail.com@5372ee58-2935-11de-9f0d-3bffa153b7de>
2010-08-25 15:33:02 +0000

    First Stable release

---
+++ tags/v-1.0/tester-arangi/src/br/ufmg/lcc/arangitester/arangi/pages/ArangiTabularPage.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2000 Universidade Federal de Minas Gerais.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package br.ufmg.lcc.arangitester.arangi.pages;
+
+import java.util.Iterator;
+import java.util.List;
+
+import br.ufmg.lcc.arangitester.arangi.ui.GenericLine;
+import br.ufmg.lcc.arangitester.ioc.UiComponentFactory;
+import br.ufmg.lcc.arangitester.ui.IUiLine;
+import br.ufmg.lcc.arangitester.ui.IUiTable;
+import br.ufmg.lcc.arangitester.ui.UiTable;
+
+/**
+ * Generic Tabular Page. It wraps a LccUiTable.
+ * @author Lucas Gonçalves
+ *
+ * @param <T> Type of line.
+ */
+public class ArangiTabularPage<T extends GenericLine> extends ArangiPage implements IUiTable<T>{
+
+	private UiTable<T> table;
+	
+	private Class<T> lineType;
+	
+	/**
+	 * Line type.
+	 * @param lineType type of line. This must be passed, for instanciated new lines.
+	 */
+	public ArangiTabularPage(Class<T> lineType) {
+		super();
+		this.lineType = lineType;
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public UiTable<T> getTable() {
+		if ( table == null ){
+			table = UiComponentFactory.getInstance(UiTable.class);
+			table.setType(lineType);
+		}
+		return table;
+	}
+	
+	public void verifyAllPreviewslyActions(){
+		getTable().verifyAllPreviewslyActions();
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public void verifyAllEnabled(boolean enabled,  Class... components) {
+		getTable().verifyAllEnabled(enabled, components);
+	}
+	
+	//
+	// DELEGATE METHOD FOR TABLE
+	//
+	@Override
+	public void removeLine(int number) {
+		getTable().removeLine(number);
+		
+	}
+
+	@Override
+	public void verifyBodyLines(int number) {
+		getTable().verifyBodyLines(number);
+	}
+
+	@Override
+	public List<T> getLines() {
+		return getTable().getLines();
+	}
+
+	@Override
+	public T addLine(boolean begin) {
+		return getTable().addLine(begin);
+	}
+
+	@Override
+	public T getLine(int i) {
+		return getTable().getLine(i);
+	}
+
+	@Override
+	public Class<T> getType() {
+		return getTable().getType();
+	}
+
+	@Override
+	public void setType(Class<T> type) {
+		getTable().setType(type);
+	}
+
+	@Override
+	public void verifyColumnText(int line, int col,	Section section, String text) {
+		getTable().verifyColumnText(line, col, section, text);
+	}
+
+	@Override
+	public int getRealLinesNumber() {
+		return getTable().getRealLinesNumber();
+	}
+
+	@Override
+	public Iterator<IUiLine> createRealLinesIterator() {
+		return getTable().createRealLinesIterator();
+	}
+}
---
+++ tags/v-1.0/tester-core/src/br/ufmg/lcc/arangitester/db/DbUnitController.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2000 Universidade Federal de Minas Gerais.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package br.ufmg.lcc.arangitester.db;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+import br.ufmg.lcc.arangitester.annotations.Db;
+import br.ufmg.lcc.arangitester.config.ConfigDatabase;
+import br.ufmg.lcc.arangitester.config.ConfigDumpFile;
+import br.ufmg.lcc.arangitester.config.ConfigFactory;
+import br.ufmg.lcc.arangitester.exceptions.EnvException;
+import br.ufmg.lcc.arangitester.exceptions.TesterException;
+import br.ufmg.lcc.arangitester.util.TimerHelper;
+
+/**
+ * Class responsible to control database dump.
+ * 
+ * @author Lucas Gonçalves
+ * 
+ */
+public class DbUnitController {
+	private static Logger	LOG	= Logger.getLogger(DbUnitController.class);
+
+	/**
+	 * Carrega todos os drivers de todos os bancos de dados configurados no arquivo tester-config.xml
+	 */
+	public DbUnitController() {
+		List<ConfigDatabase> databases = ConfigFactory.getEnvSpecificConfig().getDatabases();
+
+		if (databases != null) {
+			for (ConfigDatabase database : databases) {
+				try {
+					Class.forName(database.getDriver());
+				} catch (ClassNotFoundException e) {
+					throw new TesterException("A classe de controle do banco de dados especificada no arquivo tester-config.xml não existe");
+				}
+			}
+		}
+	}
+
+	/**
+	 * Permite fazer o reload/export fora de uma bateria de testes.
+	 * 
+	 * @param args
+	 *            [0] Ação reload/export <br>
+	 *            [1] Nome do arquivo de dump ou nulo para reload de todos.
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		if (args.length == 0 || (!args[0].equals("reload") && !args[0].equals("export"))) {
+			throw new EnvException("Deve ser passado pelo menos 1 parametro: reload ou export");
+		}
+
+		DbUnitController controller = new DbUnitController();
+		if (args[0].equals("reload")) {
+			if (args.length == 1)
+				controller.verifyWitchFileToApply((String[]) null, false);
+			else {
+				String[] files = Arrays.copyOfRange(args, 1, args.length);
+				controller.verifyWitchFileToApply(files, false);
+			}
+		} else {
+			if (args.length == 1)
+				controller.verifyWitchFileToApply((String[]) null, true);
+			else {
+				String[] files = Arrays.copyOfRange(args, 1, args.length);
+				controller.verifyWitchFileToApply(files, true);
+			}
+		}
+
+	}
+
+	/**
+	 * Apartir de uma classe de teste pega quais arquivos de dump precisão ser reloaded.
+	 * 
+	 * @param clazz
+	 *            Classe de teste.
+	 */
+	public void reload(Class<?> clazz) {
+		Db lccDb = clazz.getAnnotation(Db.class);
+		if (lccDb != null) {
+			verifyWitchFileToApply(lccDb.value(), false);
+		}
+	}
+
+	public void verifyWitchFileToApply(String[] dumpFileName, boolean export) {
+		List<ConfigDatabase> databases = ConfigFactory.getEnvSpecificConfig().getDatabases();
+		if (dumpFileName == null || dumpFileName.length == 0) { // Reload todas os arquivos de dump configurados no arquivo tester-config.xml
+			if (databases != null) {
+				for (ConfigDatabase database : databases) {
+					boolean databaseReloaded = false;
+					for (ConfigDumpFile dumpFile : database.getFile()) {
+						if (export) {
+							export(database, dumpFile);
+						} else {
+							databaseReloaded = true;
+							reload(database, dumpFile);
+						}
+					}
+					if (databaseReloaded) {
+						this.executeListener(database);
+					}
+				}
+			}
+		} else {
+			for (String toLoad : dumpFileName) { // Reload APENAS os arquivos de dump passados na linha de comando.
+				for (ConfigDatabase database : databases) {
+					boolean databaseReloaded = false;
+					for (ConfigDumpFile dumpFile : database.getFile()) {
+						if (dumpFile.getName().equals(toLoad) || dumpFile.getName().equals(toLoad)) {
+							if (export) {
+								export(database, dumpFile);
+							} else {
+								databaseReloaded = true;
+								reload(database, dumpFile);
+							}
+							break;
+						}
+					}
+					if (databaseReloaded) {
+						this.executeListener(database);
+					}
+				}
+			}
+		}
+	}
+
+	private void executeListener(ConfigDatabase database) {
+		if (StringUtils.isNotBlank(database.getListener())) {
+			try {
+				Class<?> listenerClass = Class.forName(database.getListener());
+				IReloadListener newInstance = (IReloadListener) listenerClass.newInstance();
+				newInstance.reload(database);
+			} catch (Exception e) {
+				throw new EnvException(String.format("Error executing listener %s", database.getListener()), e);
+			}
+		}
+	}
+
+	private void export(ConfigDatabase database, ConfigDumpFile dumpFile) {
+		TimerHelper timer = new TimerHelper();
+		LOG.info("Exportando schema " + dumpFile.getName() + ":" + dumpFile.getName() + ".xml");
+		try {
+			DriverDb db = null;
+			if (database.getDriver().contains("oracle")) {
+				db = new OracleDb();
+			} else if (database.getDriver().contains("postgres")) {
+				db = new PostgresDb();
+			} else if (database.getDriver().contains("mysql")) {
+				db = new MysqlDb();
+			} else if (database.getDriver().contains("sqlserver")) {
+				db = new SqlServerDb();
+			}
+			db.export(database, dumpFile);
+			LOG.info("Esquema exportado com sucesso!");
+			LOG.info("Tempo gasto: " + timer.getMin() + "min " + timer.getSec() + "seg");
+		} catch (Exception e) {
+			throw new EnvException("Error ao tentar exportar o banco " + dumpFile.getName() + " : " + dumpFile.getName() + ".xml", e);
+		}
+	}
+
+	private void reload(ConfigDatabase database, ConfigDumpFile schema) {
+		TimerHelper timer = new TimerHelper();
+		LOG.info("Recarregando schema " + schema.getName() + ":" + schema.getName() + ".xml");
+		try {
+			DriverDb db = null;
+			if (database.getDriver().contains("oracle")) {
+				db = new OracleDb();
+			} else if (database.getDriver().contains("postgres")) {
+				db = new PostgresDb();
+			} else if (database.getDriver().contains("mysql")) {
+				db = new MysqlDb();
+			} else if (database.getDriver().contains("sqlserver")) {
+				db = new SqlServerDb();
+			}
+			db.reload(database, schema);
+			LOG.info("Esquema recarregado com sucesso!");
+			LOG.info("Tempo gasto: " + timer.getMin() + "min " + timer.getSec() + "seg");
+		} catch (Exception e) {
+			throw new EnvException("Error ao tentar recarregar o banco " + schema.getName() + " : " + schema.getName() + ".xml", e);
+		}
+	}
+
+}
