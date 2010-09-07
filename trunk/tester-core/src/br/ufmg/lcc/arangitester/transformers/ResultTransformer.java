09df881b9d80683fe733cc0609825f5a64615c98
goncalvesl@gmail.com <goncalvesl@gmail.com@5372ee58-2935-11de-9f0d-3bffa153b7de>
2010-09-07 14:51:42 +0000

    Fixing @login on methods

--- trunk/tester-core/src/br/ufmg/lcc/arangitester/transformers/ResultTransformer.java
+++ trunk/tester-core/src/br/ufmg/lcc/arangitester/transformers/ResultTransformer.java
@@ -2,21 +2,19 @@ package br.ufmg.lcc.arangitester.transformers;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
-public class Transformer {
+public class ResultTransformer {
 
 	public static void tranform(File fileIn, File fileOut, InputStream xslIs) throws Exception {
+
 		FileOutputStream out = new FileOutputStream(fileOut);
 		Result bufferResultado = new StreamResult(out);
 
@@ -25,16 +23,9 @@ public class Transformer {
 		TransformerFactory fabricaTrans = TransformerFactory.newInstance();
 		try {
 			javax.xml.transform.Transformer transformador = fabricaTrans.newTransformer(xsltSource);
-			try {
-				transformador.transform(xmlSource, bufferResultado);
-			} catch (TransformerException ex) {
-				ex.printStackTrace();
-			}
-
-		} catch (TransformerConfigurationException ex) {
-			ex.printStackTrace();
+			transformador.transform(xmlSource, bufferResultado);
+		} finally {
+			out.close();
 		}
-		out.close();
 	}
-
 }
